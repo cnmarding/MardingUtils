8d8d9cc70091f800764d68011e5bc293acef76cf
Administrator <Administrator@DKH>
2018-04-20 15:37:10 +0800

    WowKaOkay 提交

--- src/cn/marding/utils/MardingList.java
+++ src/cn/marding/utils/MardingList.java
@@ -7,11 +7,13 @@ import java.util.Iterator;
  * 码丁集合1.0 更加完善的有序数据存储集合
  * 
  * @version 1.0
- * @author Antony
+ * @author WowkaOkay
  * @since 2018-4-18
  * 
  * 
  */
+
+
 public class MardingList<W> implements Iterable {
 	// 用来存放数据
 	public Object[] elementData;
